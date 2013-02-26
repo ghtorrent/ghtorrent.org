d6b6923fcabc5a78a078b607d824bb41be900098
Georgios Gousios <gousiosg@gmail.com>
2013-02-26 14:13:19 +0100

    Dump 2013-02-26

--- dumps/run-all.sh
+++ dumps/run-all.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-./ght-periodic-dump -f '2012-01-31 00:00' -t '2012-03-31 00:00'
+#./ght-periodic-dump -f '2012-01-31 00:00' -t '2012-03-31 00:00'
+#./update-downloads.sh
+
+#./ght-periodic-dump -f '2012-03-31 00:00' -t '2012-05-31 00:00'
+#./update-downloads.sh
+
+#./ght-periodic-dump -f '2012-05-31 00:00' -t '2012-07-31 00:00'
+#./update-downloads.sh
+
+./ght-periodic-dump -f '2012-07-31 00:00' -t '2012-09-30 00:00'
 ./update-downloads.sh
 
-./ght-periodic-dump -f '2012-03-31 00:00' -t '2012-05-31 00:00'
+./ght-periodic-dump -f '2012-09-30 00:00' -t '2012-11-30 00:00'
 ./update-downloads.sh
 
-./ght-periodic-dump -f '2012-05-31 00:00' -t '2012-07-31 00:00'
-./update-downloads.sh
-
-./ght-periodic-dump -f '2012-07-31 00:00' -t '2012-09-31 00:00'
-./update-downloads.sh
-
-./ght-periodic-dump -f '2012-09-31 00:00' -t '2012-11-31 00:00'
-./update-downloads.sh
-
-./ght-periodic-dump -f '2012-11-31 00:00' -t '2013-01-31 00:00'
+./ght-periodic-dump -f '2012-11-30 00:00' -t '2013-01-30 00:00'
 ./update-downloads.sh
